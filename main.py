809be291e21bd7e2c806484e92815155c1a641a8
Kostas <kostastsale@gmail.com>
2020-11-20 07:18:55 +0000

    Initial Commit
    Initial Commit
    
    Update README.md
    
    Update README.md
    
    Delete file.json
    
    Delete poetry.lock
    
    Update pyproject.toml
    
    Update main.py
    
    Update main.py

--- main.py
+++ main.py
@@ -3,35 +3,45 @@ import json
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", help="-Specify the full path of the json file", required=False)
+parser.add_argument(
+    "-f",
+    "--file",
+    help="-Specify the full path of the json file",
+    required=True)
 args = parser.parse_args()
-file = args.file
 
-lift = attack_client()
-file = "file.json"
 
-all_techniques = lift.get_techniques()
-lst = []
-subs = []
-result_lst = list()
 
-with open(file, "r+") as jsonfile:
-    file = json.load(jsonfile)
-    for i in file["techniques"]:
-        if i["showSubtechniques"] is True:
-            continue
-        else:
-            lst.append(i["techniqueID"])
-    lst = list(dict.fromkeys(lst))
+def get_techniques():
+    file = args.file
+    techniques = []
+    with open(file, "r+") as jsonfile:
+        file = json.load(jsonfile)
+        for i in file["techniques"]:
+            if i["showSubtechniques"] is True:
+                continue
+            else:
+                techniques.append(i["techniqueID"])
+        techniques = list(dict.fromkeys(techniques))
+        return techniques
+
 
 def get_techniques_info_by_ID(ID):
     for name in all_techniques:
         if ID.lower() in name['external_references'][0]['external_id'].lower():
-            result_lst.append(
-                f"""<a href="{name['external_references'][0]['url']}">[MITRE ATT&amp;CK] {name["name"]} - {ID}</a> """)
+            fin_lst.append(
+                f"""<a href="{name['external_references'][0]['url']}">[MITRE ATT&amp;CK] {name["name"]} - {ID}</a><br> """
+            )
 
 
-for x in lst:
-    get_techniques_info_by_ID(x)
+if __name__ == "__main__":
+    fin_lst = []
+    techniques = get_techniques()
+    lift = attack_client()
+    all_techniques = lift.get_techniques()
 
-print("\n" + f"<p><strong>MITRE ATT&amp;CK:</strong> {' | '.join(result_lst)}")
+    for x in techniques:
+        get_techniques_info_by_ID(x)
+
+    print("\n" +
+          f"<p><strong>MITRE ATT&amp;CK:</strong> {' '.join(fin_lst)}")
